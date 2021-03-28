f1fc2330d9aeafd238fbf7bde742af2d68095fbf
roufy235 <roufy235@gmail.com>
2021-03-28 15:43:31 +0100

    Set-up

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+
+    float pi = 3.14;
+    int number;
+    std::cout << "Please enter a value for number" << std::endl;
+    std::cin >> number; // this will receive value from the console
+    char name[] = "Bello Abdulrouf";
+    bool isLoading = false;
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
